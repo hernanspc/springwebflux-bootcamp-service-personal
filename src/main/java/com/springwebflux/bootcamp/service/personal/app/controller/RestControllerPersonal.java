8f8008821c03de82fb03c0265ccbb0b80728c0d8
hernanspc <iphone1198@icloud.com>
2019-12-18 11:02:51 -0500

    add

--- src/main/java/com/springwebflux/bootcamp/service/personal/app/controller/RestControllerPersonal.java
+++ src/main/java/com/springwebflux/bootcamp/service/personal/app/controller/RestControllerPersonal.java
@@ -1,5 +1,4 @@
 package com.springwebflux.bootcamp.service.personal.app.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +8,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.springwebflux.bootcamp.service.personal.app.model.Personal;
 import com.springwebflux.bootcamp.service.personal.app.services.PersonalServicesImplement;
-
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -23,7 +20,7 @@ public class RestControllerPersonal {
 	 PersonalServicesImplement implement;
 	  
 	  @GetMapping("/getPersonal") 
-	  Flux<Personal>getBusiness()
+	  Flux<Personal>getPersonal()
 	 	{
 	 	return implement.getPersonal();
 	 	}	
